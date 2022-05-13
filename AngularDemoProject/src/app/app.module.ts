3b19fcb269f6d0e9334e22e92cb00aef31b20519
unknown <firdoushjamil@gmail.com>
2022-05-13 12:24:44 +0530

    initial commit

---
+++ AngularDemoProject/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './common/header/header.component';
+import { ResourceComponent } from './resource/resource.component';
+import { NextstepsComponent } from './nextsteps/nextsteps.component';
+import { FooterComponent } from './common/footer/footer.component';
+import { LinksComponent } from './links/links.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ResourceComponent,
+    NextstepsComponent,
+    FooterComponent,
+    LinksComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
